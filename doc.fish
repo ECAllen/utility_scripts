eb5f36c150c7e924713978a8872b36aaa2e7f97a
Ethan Allen <ethancallen@gmail.com>
2017-04-19 14:22:46 -0400

    updated doc.fish for line wrapping

--- doc.fish
+++ doc.fish
@@ -1,9 +1,11 @@
 #! /usr/bin/fish
 
+# For starting godoc at wm startup
+
 nohup godoc -http=:6060 >/dev/null &
 
 set msg (fortune -a)
 
 set gdoc "godoc started on port 6060"
 
-xmessage $msg $gdoc 
+echo -e "$msg\n\n$gdoc" | fold -s | xmessage -file - 
